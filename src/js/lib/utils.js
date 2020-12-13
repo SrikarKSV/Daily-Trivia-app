e23db4f5da7e43673de8b421d6a352576cd23e41
Srikar KSV <srikar1awesome@gmail.com>
2020-12-13 11:38:59 +0530

    📦 NEW: Added a countdown sequence

--- src/js/lib/utils.js
+++ src/js/lib/utils.js
@@ -1,6 +1,11 @@
 import categories from "../data/categories";
-
-const selectCategoryInput = document.querySelector("#category");
+import {
+  gameContainer,
+  countDownContainer,
+  menuContainer,
+  spanCountDown,
+  selectCategoryInput,
+} from "./elements";
 
 export function renderCategories() {
   const optionsHTML = categories.map(
@@ -8,4 +13,35 @@ export function renderCategories() {
       `<option value="${categories.id}">${categories.name}</option>`
   );
   selectCategoryInput.insertAdjacentHTML("beforeend", optionsHTML);
+}
+
+export function restartGame() {
+  menuContainer.classList.toggle("not-active");
+  setTimeout(() => {
+    gameContainer.classList.remove("expand");
+  }, 500);
+}
+
+export function startCountdown() {
+  countDownContainer.classList.add("active");
+  let count = 3;
+  const countDownInterval = setInterval(() => {
+    // Fading the countdown
+    spanCountDown.classList.add("go-up");
+    spanCountDown.addEventListener(
+      "transitionend",
+      () => {
+        spanCountDown.innerText = --count;
+        spanCountDown.classList.remove("go-up");
+      },
+      { once: true }
+    );
+    if (count < 2) {
+      clearInterval(countDownInterval);
+      // Resetting the countdown for restart
+      spanCountDown.innerText = 3;
+      spanCountDown.classList.remove("go-up");
+      countDownContainer.classList.remove("active");
+    }
+  }, 1000);
 }
