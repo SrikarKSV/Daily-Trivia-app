d9d67744a2ea6fb532e4b4f91789c6a946bc1da5
Srikar KSV <srikar1awesome@gmail.com>
2020-12-13 11:38:02 +0530

    📦 NEW: Selected all the elements in a single module

---
+++ src/js/lib/elements.js
@@ -0,0 +1,5 @@
+export const gameContainer = document.querySelector(".trivia-container");
+export const countDownContainer = document.querySelector(".countdown");
+export const spanCountDown = document.querySelector(".countdown span");
+export const menuContainer = document.querySelector(".setup-container");
+export const selectCategoryInput = document.querySelector("#category");
