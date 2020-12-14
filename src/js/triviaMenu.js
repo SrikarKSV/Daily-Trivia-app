daaa0998ddbd91d1351c6987d27818f826b96168
Srikar KSV <srikar1awesome@gmail.com>
2020-12-14 17:01:06 +0530

    👌 IMPROVE: Added a loading animation when API is requested

--- src/js/triviaMenu.js
+++ src/js/triviaMenu.js
@@ -1,6 +1,6 @@
 import getQuestions from "./lib/fetchQuestions";
 import { restartGame } from "./lib/utils";
-import { gameContainer } from "./lib/elements";
+import { gameContainer, loadingAnimation } from "./lib/elements";
 import { trviaStart } from "./triviaGame";
 
 let results;
@@ -8,7 +8,9 @@ let results;
 export async function handleMenuSubmit(e) {
   e.preventDefault();
   const form = e.currentTarget;
+  loadingAnimation.classList.add("active");
   results = await getQuestions(form.category.value, form.difficulty.value);
+  loadingAnimation.classList.remove("active");
   restartGame();
   gameContainer.dispatchEvent(new CustomEvent("gameStarted"));
 }
